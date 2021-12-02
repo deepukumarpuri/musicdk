5c109d8184a3fcd63aaa588121ee8638aa451937
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-12-02 08:24:48 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo, Please Wait..."
+git clone https://github.com/deepukumarpuri/MUSIC12 /MUSIC12
+cd /MUSIC12
+pip3 install -U -r requirements.txt
+echo "Starting Bot, Please Wait..."
+python3 main.py
